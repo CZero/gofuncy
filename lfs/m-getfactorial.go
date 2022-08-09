737f47564d4ef9404978e37f9dddf7425d378089
Steph <steph@taizer.net>
2022-08-09 14:30:16 +0200

    Create m-getfactorial.go
    Factorials functie toegevoegd

---
+++ lfs/m-getfactorial.go
@@ -0,0 +1,13 @@
+package lfs
+
+// GetFactorial returns the factorials of a number. It returns 0 when 0 or smaller.
+func GetFactorial(input int) int {
+	if input <= 0 {
+		return 0
+	}
+	result := 1
+	for i := 1; i <= input; i++ {
+		result *= (i)
+	}
+	return result
+}
